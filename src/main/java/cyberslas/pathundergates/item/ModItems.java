7e21cf3e71f78d260b4f88bfea1543777a09f51a
Cyberslas <littlej54192@gmail.com>
2020-10-10 05:27:20 -0700

    Fix from cherrypicking 1.16 commit

--- src/main/java/cyberslas/pathundergates/item/ModItems.java
+++ src/main/java/cyberslas/pathundergates/item/ModItems.java
@@ -5,14 +5,13 @@ import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Items;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
-    private static final DeferredRegister<Item> ITEMBLOCKS_OVERRIDE = DeferredRegister.create(ForgeRegistries.ITEMS, Blocks.GRASS_PATH.getRegistryName().getNamespace());
+    private static final DeferredRegister<Item> ITEMBLOCKS_OVERRIDE = new DeferredRegister<>(ForgeRegistries.ITEMS, Blocks.GRASS_PATH.getRegistryName().getNamespace());
 
     public static final RegistryObject<Item> PUG_GRASS_PATH = ITEMBLOCKS_OVERRIDE.register(Blocks.GRASS_PATH.getRegistryName().getPath(), () -> new BlockItem(ModBlocks.PUG_GRASS_PATH.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
 
