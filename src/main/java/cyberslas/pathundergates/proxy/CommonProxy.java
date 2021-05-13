13ba7fb2201779719278592afb1cf73abf1450c7
Cyberslas <littlej54192@gmail.com>
2021-05-13 03:01:04 -0700

    Guess this event was needed to update single-player config when changed

--- src/main/java/cyberslas/pathundergates/proxy/CommonProxy.java
+++ src/main/java/cyberslas/pathundergates/proxy/CommonProxy.java
@@ -1,11 +1,13 @@
 package cyberslas.pathundergates.proxy;
 
 import cyberslas.pathundergates.event.EventHandler;
+import cyberslas.pathundergates.PUGConfig;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent e) {
+        MinecraftForge.EVENT_BUS.register(PUGConfig.class);
         MinecraftForge.EVENT_BUS.register(EventHandler.class);
     }
 }
--- src/main/java/cyberslas/pathundergates/proxy/ServerProxy.java
+++ src/main/java/cyberslas/pathundergates/proxy/ServerProxy.java
@@ -1,15 +1,8 @@
 package cyberslas.pathundergates.proxy;
 
-import cyberslas.pathundergates.PUGConfig;
-import cyberslas.pathundergates.event.EventHandler;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod.EventBusSubscriber
 public class ServerProxy extends CommonProxy {
-    public void preInit(FMLPreInitializationEvent e) {
-        super.preInit(e);
-        MinecraftForge.EVENT_BUS.register(PUGConfig.class);
-    }
 }
