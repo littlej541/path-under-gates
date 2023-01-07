9019aa7ae545ede487e6c0fc9fc48635a02c4048
Cyberslas <littlej54192@gmail.com>
2023-01-07 05:17:40 -0800

    Slightly more topical examples

--- src/main/java/cyberslas/pathundergates/Config.java
+++ src/main/java/cyberslas/pathundergates/Config.java
@@ -34,7 +34,7 @@ public class Config {
                             "The first block can only be a simple block, properties are ignored. The second can be a block with properties attached like the blacklist/whitelist.",
                             "Blocks pairs are separated by a | character. Cannot use tags or wildcards. Properties not defined are assumed default. Format: modid:name|modid:name:propertyname1=propertyvalue1,propertyname2=propertyvalue2,...",
                             "Intended for compatibility purposes, but nothing stopping you from going nuts and making shovels turn coal blocks into diamonds.",
-                            "Examples: \"minecraft:granite|minecraft:stone\", \"minecraft:oak_stairs|minecraft:oak_stairs:half=top\", \"minecraft:netherrack|somemod:netherrack_path\"",
+                            "Examples: \"minecraft:snow_block|morepaths:snow_path\", \"undergarden:deepsoil|ugpaths:deepsoil_path\", \"minecraft:coal_block|minecraft:diamond_block\"",
                             "Each entry must be surrounded by double quotes")
                     .translation(PathUnderGates.MODID + ".config.blockPathPairList")
                     .defineList("blockPathPairList", new ArrayList<String>(), __ -> true);
