81f0a0aadd28ffde6309c35b75d68e0118e3629a
Cyberslas <littlej54192@gmail.com>
2023-01-08 11:04:06 -0800

    Simpler method for registering paths by IMC

--- src/main/java/cyberslas/pathundergates/PathUnderGates.java
+++ src/main/java/cyberslas/pathundergates/PathUnderGates.java
@@ -9,12 +9,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Mod(PathUnderGates.MODID)
 public class PathUnderGates {
@@ -29,28 +25,17 @@ public class PathUnderGates {
     }
 
     private void processComms(InterModProcessEvent event) {
-        Map<String, ImmutablePair<Block, BlockState>> messageMap = new HashMap<>();
+        for(InterModComms.IMCMessage message : InterModComms.getMessages(MODID).toList()) {
+            if (message.method().equals("registerpath")) {
+                Object rawMessageData = message.messageSupplier().get();
+                if (rawMessageData instanceof Object[]) {
+                    Object[] arrayData = (Object[])rawMessageData;
 
-        InterModComms.getMessages(MODID).forEach(message -> {
-            String messageKey = message.method();
-            Object messageData = message.messageSupplier().get();
-
-            ImmutablePair<Block, BlockState> pair = messageMap.computeIfAbsent(messageKey, (whatever) -> ImmutablePair.nullPair());
-
-            if (messageData instanceof Block) {
-                messageMap.put(messageKey, ImmutablePair.of((Block)messageData, pair.getRight()));
-            } else if (messageData instanceof BlockState) {
-                messageMap.put(messageKey, ImmutablePair.of(pair.getLeft(), (BlockState)messageData));
+                    if (arrayData.length > 1 && arrayData[0] instanceof Block && arrayData[1] instanceof BlockState) {
+                        ParsedConfig.addBlockPathMapping((Block)arrayData[0], (BlockState)arrayData[1]);
+                    }
+                }
             }
-        });
-
-        messageMap.values().forEach(pair -> {
-            Block block = pair.getLeft();
-            BlockState blockState = pair.getRight();
-
-            if (block != null && blockState != null) {
-                ParsedConfig.addBlockPathMapping(block, blockState);
-            }
-        });
+        }
     }
 }
